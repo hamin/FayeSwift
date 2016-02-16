b8af8b6eb44cde0c2a8e950af30906f4e7af8a1e
Shams Ahmed <shamsahmed@me.com>
2016-02-16 12:44:21 +0000

    removed some console logs and replaced with the delegate method

--- Example/FayeSwift/ViewController.swift
+++ Example/FayeSwift/ViewController.swift
@@ -14,14 +14,15 @@ class ViewController: UIViewController, UITextFieldDelegate, FayeClientDelegate 
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var textView: UITextView!
   
-  let client: FayeClient = FayeClient(aFayeURLString: "ws://localhost:5222/faye", channel: "/cool")
+    // ws://localhost:5222/faye
+  let client: FayeClient = FayeClient(aFayeURLString: "wss://fusion.fusion-universal.com:8080/faye", channel: "/cool")
   
   // MARK:
   // MARK: Lifecycle
     
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
     client.delegate = self;
     client.connectToServer()
     
@@ -29,32 +30,25 @@ class ViewController: UIViewController, UITextFieldDelegate, FayeClientDelegate 
       let text: AnyObject? = messageDict["text"]
       print("Here is the Block message: \(text)")
     }
+    
     client.subscribeToChannel("/awesome", block: channelBlock)
     
-    
-    
-    let delayTime = dispatch_time(DISPATCH_TIME_NOW,
-      Int64(3 * Double(NSEC_PER_SEC)))
-    dispatch_after(delayTime, dispatch_get_main_queue()) {
-      print("unsub")
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(3 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
       self.client.unsubscribeFromChannel("/awesome")
     }
     
-    _ = dispatch_time(DISPATCH_TIME_NOW,
-      Int64(5 * Double(NSEC_PER_SEC)))
-    dispatch_after(delayTime, dispatch_get_main_queue()) {
-      print("resub")
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+      print("resubscribe to awesome")
       self.client.subscribeToChannel("/awesome", block: channelBlock)
     }
-    
   }
   
   // MARK:
   // MARK: TextfieldDelegate
 
   func textFieldShouldReturn(textField: UITextField) -> Bool {
-    // client.sendMessage(["text": textField.text], channel: "/cool")
     client.sendMessage(["text" : textField.text as! AnyObject], channel: "/cool")
+
     return false;
   }
   
@@ -65,24 +59,24 @@ class ViewController: UIViewController, UITextFieldDelegate, FayeClientDelegate 
     print("Connected to Faye server")
   }
   
-  func connectionFailed() {
-    print("Failed to connect to Faye server!")
+  func connectionFailed(error: NSError?) {
+    print("Failed to connect to Faye server with \(error)")
   }
   
-  func disconnectedFromServer() {
+  func disconnectedFromServer(error: NSError) {
     print("Disconnected from Faye server")
   }
   
   func didSubscribeToChannel(channel: String) {
-    print("subscribed to channel \(channel)")
+    print("Subscribed to channel \(channel)")
   }
   
   func didUnsubscribeFromChannel(channel: String) {
-    print("UNsubscribed from channel \(channel)")
+    print("Unsubscribed from channel \(channel)")
   }
   
   func subscriptionFailedWithError(error: String) {
-    print("SUBSCRIPTION FAILED!!!!")
+    print("Subscription failed with \(error)")
   }
   
   func messageReceived(messageDict: NSDictionary, channel: String) {

c33f1f599597f0d216a8ea358d0bd08e7d7813a1
Ronan Mchugh <rmchugh@zendesk.com>
2020-03-27 16:36:16 +0000

    again

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
                 dependencies: [._byNameItem(name: "Starscream")],
                 path: "./Sources",
                 exclude: [],
-                sources: nil)
+                sources: ["./Sources"])
     ]
 )

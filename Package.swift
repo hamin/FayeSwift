d6db079b1c87ebc56a0b855cdf0eb3c894e7328d
Ronan Mchugh <rmchugh@zendesk.com>
2020-03-27 13:56:29 +0000

    try

--- Package.swift
+++ Package.swift
@@ -3,8 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "FayeSwift",
+    products: [
+        .library(name: "FayeClient", targets: ["Faye"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0")
     ],
-    targets: []
+    targets: [
+    .systemLibrary(name: "Faye", path: "Sources", pkgConfig: nil, providers: nil)
+    ]
 )
